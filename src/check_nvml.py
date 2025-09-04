8282cbca007b0a517fa09baea84caf254fe7d6c3
Moatasem Nada <mkhgnada@uwaterloo.ca>
2025-09-04 19:39:45 -0400

    chore: set up Phase 0 and clean files

---
+++ src/check_nvml.py
@@ -0,0 +1,49 @@
+from pynvml import *
+
+
+try: 
+    nvmlInit()
+    print("NVML intialized")
+
+
+
+    Driver = nvmlSystemGetDriverVersion()
+
+
+
+    for i in range(nvmlDeviceGetCount()):
+        handle = nvmlDeviceGetHandleByIndex(i)
+        name = nvmlDeviceGetName(handle)
+        uuid = nvmlDeviceGetUUID(handle)
+        memory = nvmlDeviceGetMemoryInfo(handle)
+        used = memory.used / 1048576.0
+        total = memory.total / 1048576.0
+
+        try:
+            power = nvmlDeviceGetPowerUsage(handle) / 1000
+        except NVML_ERROR_NOT_SUPPORTED:
+            print("NVML_ERROR_NOT_SUPPORTED")
+            power = None
+            
+        try: 
+            temp = nvmlDeviceGetTemperature(handle, NVML_TEMPERATURE_GPU)
+        except NVML_ERROR_NOT_SUPPORTED:
+            print("NVML_ERROR_NOT_SUPPORTED")
+            temp = None 
+
+
+        print(f"[GPU {i}] {name}")
+        print(f"UUID           : {uuid}")
+        print(f"Driver Version : {Driver}")
+        print(f"Memory Usage   : {used} / {total} MB")
+        print(f"Power          : {power} W")
+        print(f"Temp           : {temp} C")
+
+
+except NVMLError:
+    print("ERROR: NVML failed to initialize. Is the NVIDIA driver installed?")
+
+finally:
+    try: nvmlShutdown()
+    except Exception: pass
+
